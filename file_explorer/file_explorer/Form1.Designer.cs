37dfd43017009a18f845221f14bcc58b817cc5db
Alaa Yacoub <41425443+alaaelbarasi@users.noreply.github.com>
2022-01-29 20:44:52 +0200

    Add files via upload

---
+++ file_explorer/file_explorer/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace file_explorer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dirListBox1 = new Microsoft.VisualBasic.Compatibility.VB6.DirListBox();
+            this.driveListBox1 = new Microsoft.VisualBasic.Compatibility.VB6.DriveListBox();
+            this.fileListBox1 = new Microsoft.VisualBasic.Compatibility.VB6.FileListBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.createToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pasteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dirListBox1
+            // 
+            this.dirListBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.dirListBox1.FormattingEnabled = true;
+            this.dirListBox1.IntegralHeight = false;
+            this.dirListBox1.Location = new System.Drawing.Point(81, 81);
+            this.dirListBox1.Name = "dirListBox1";
+            this.dirListBox1.Size = new System.Drawing.Size(321, 343);
+            this.dirListBox1.TabIndex = 0;
+            this.dirListBox1.SelectedIndexChanged += new System.EventHandler(this.dirListBox1_SelectedIndexChanged);
+            // 
+            // driveListBox1
+            // 
+            this.driveListBox1.FormattingEnabled = true;
+            this.driveListBox1.Location = new System.Drawing.Point(81, 52);
+            this.driveListBox1.Name = "driveListBox1";
+            this.driveListBox1.Size = new System.Drawing.Size(321, 23);
+            this.driveListBox1.TabIndex = 1;
+            this.driveListBox1.SelectedIndexChanged += new System.EventHandler(this.driveListBox1_SelectedIndexChanged);
+            // 
+            // fileListBox1
+            // 
+            this.fileListBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.fileListBox1.FormattingEnabled = true;
+            this.fileListBox1.Location = new System.Drawing.Point(408, 81);
+            this.fileListBox1.Name = "fileListBox1";
+            this.fileListBox1.Pattern = "*.*";
+            this.fileListBox1.Size = new System.Drawing.Size(610, 340);
+            this.fileListBox1.TabIndex = 2;
+            this.fileListBox1.SelectedIndexChanged += new System.EventHandler(this.fileListBox1_SelectedIndexChanged);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.createToolStripMenuItem,
+            this.copyToolStripMenuItem,
+            this.pasteToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(173, 76);
+            // 
+            // createToolStripMenuItem
+            // 
+            this.createToolStripMenuItem.Name = "createToolStripMenuItem";
+            this.createToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.createToolStripMenuItem.Size = new System.Drawing.Size(172, 24);
+            this.createToolStripMenuItem.Text = "create";
+            this.createToolStripMenuItem.Click += new System.EventHandler(this.createToolStripMenuItem_Click);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.C)));
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(172, 24);
+            this.copyToolStripMenuItem.Text = "copy";
+            this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
+            // 
+            // pasteToolStripMenuItem
+            // 
+            this.pasteToolStripMenuItem.Name = "pasteToolStripMenuItem";
+            this.pasteToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.V)));
+            this.pasteToolStripMenuItem.Size = new System.Drawing.Size(172, 24);
+            this.pasteToolStripMenuItem.Text = "paste";
+            this.pasteToolStripMenuItem.Click += new System.EventHandler(this.pasteToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(78, 427);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1068, 501);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.fileListBox1);
+            this.Controls.Add(this.driveListBox1);
+            this.Controls.Add(this.dirListBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "File Explorer";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Microsoft.VisualBasic.Compatibility.VB6.DirListBox dirListBox1;
+        private Microsoft.VisualBasic.Compatibility.VB6.DriveListBox driveListBox1;
+        private Microsoft.VisualBasic.Compatibility.VB6.FileListBox fileListBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem createToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pasteToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ file_explorer/file_explorer/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.IO;
+//1)ShortCut
+//2)PictureBox
+namespace file_explorer
+{
+    public partial class Form1 : Form
+    {
+        string sourceFile;
+        string destinationName;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void driveListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            dirListBox1.Path = driveListBox1.Drive;
+
+        }
+
+        private void dirListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            fileListBox1.Path = dirListBox1.Path;
+            fileListBox1.Refresh();
+        }
+
+        private void fileListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label1.Text = fileListBox1.Path + "\\" + fileListBox1.FileName;
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            sourceFile = fileListBox1.Path + "\\"+fileListBox1.FileName;
+            destinationName = fileListBox1.FileName;
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            File.Copy(sourceFile,dirListBox1.Path+"\\"+destinationName);
+            fileListBox1.Refresh();
+        }
+
+        private void createToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string newFolder = Interaction.InputBox("Input folder name","Create folder","");
+            Directory.CreateDirectory(dirListBox1.Path+"\\"+newFolder);
+            dirListBox1.Refresh();
+        }
+
+
+
+
+
+
+    }
+}
